31bebd9a164438d7aeb21a40312f906dc21334da
SWATHI BHAT <SWATHI BHAT@SWATHISBHAT>
2026-03-12 12:06:31 +0530

    Commit git conflict

--- src/test/java/Practice/Demo2.java
+++ src/test/java/Practice/Demo2.java
@@ -6,7 +6,7 @@ public class Demo2 {
 		System.out.println("Practicing pull operation");
 		System.out.println("Through branches");
 		System.out.println("Pull Operation to Framework Directory");
-		System.out.println("Practicing git conflict in ATE Directory");
+		System.out.println("Git conflict in ATE directory");
 
 	}
 
